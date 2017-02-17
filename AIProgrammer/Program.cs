f0e3e212e47f9efb8aa76d787008de19a1842229
Nikita Grachev <nikita.grachev@gmail.com>
2017-02-17 16:30:49 +0700

    changed to 3*n func

--- AIProgrammer/Program.cs
+++ AIProgrammer/Program.cs
@@ -40,7 +40,7 @@ namespace AIProgrammer
         private static double _crossoverRate = 0.70; // Percentage chance that a child genome will use crossover of two parents.
         private static double _mutationRate = 0.01; // Percentage chance that a child genome will mutate a gene.
         private static int _genomeSize = 150; // Number of programming instructions in generated program (size of genome array). loops).
-        private static int _maxGenomeSize = 300; // The max length a genome may grow to (only applicable if _expandAmount > 0).
+        private static int _maxGenomeSize = 150; // The max length a genome may grow to (only applicable if _expandAmount > 0).
         private static int _maxIterationCount = 5000; // Max iterations a program may run before being killed (prevents infinite loops).
         private static int _expandAmount = 0; // The max genome size will expand by this amount, every _expandRate iterations (may help learning). Set to 0 to disable.
         private static int _expandRate = 5000; // The max genome size will expand by _expandAmount, at this interval of generations.
@@ -65,7 +65,8 @@ namespace AIProgrammer
         private static IFitness GetFitnessMethod()
         {
             //return new StringStrictFitness(_ga, _maxIterationCount, _targetParams.TargetString, _appendCode);
-            return new SimpleNumbers(_ga, _maxIterationCount);
+            return new SimpleNumbers(_ga, _maxIterationCount, 3, TimesThreeFitness.Functions);
+            //return new SimpleNumbers(_ga, _maxIterationCount);
         }
 
         #region Worker Methods
